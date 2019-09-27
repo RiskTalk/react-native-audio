36b8017d576f44af76684efd5ddacebd7fcb2720
Arjun Ghimire <arjunghimire0714@gmail.com>
2019-09-27 14:23:05 +0545

    Fixed current metering issue

--- android/src/main/java/com/rnim/rn/audio/AudioRecorderManager.java
+++ android/src/main/java/com/rnim/rn/audio/AudioRecorderManager.java
@@ -316,9 +316,12 @@ class AudioRecorderManager extends ReactContextBaseJavaModule {
         if (!isPaused) {
           WritableMap body = Arguments.createMap();
           body.putDouble("currentTime", stopWatch.getTimeSeconds());
-
-          int amplitude = recorder.getMaxAmplitude();
-          body.putInt("currentMetering",  amplitude);
+         int amplitude = recorder.getMaxAmplitude();
+           if (amplitude == 0) {
+            body.putInt("currentMetering", 0);
+          } else {
+            body.putInt("currentMetering", (int) (20 * Math.log(((double) amplitude) / 32767d)));
+          }
           sendEvent("recordingProgress", body);
         }
       }
